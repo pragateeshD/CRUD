881ec30a64221e94ed49ed5e0ab792261e3f5b17
pragateesh dharmaraj <pragateeshdr@gmail.com>
2024-07-12 20:55:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Users />} />
-        <Route path='/create' element={<CreateUser />} />
+        <Rooute path='/create' element={<CreateUser />} />
         <Route path='/update/:id' element={<UpdateUser />} />
       </Routes>
     </BrowserRouter>
