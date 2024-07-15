e7c441110c1baa60b9e36830e750ec631a4de204
pragateesh dharmaraj <pragateeshdr@gmail.com>
2024-07-15 21:43:10 +0530

    Update Users.js

--- Users.js
+++ Users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')//model for user tabel
     const UserSchema = new mongoose.Schema(
         {
             name: Strring,
-            email:Strring,
+            email:String,
             age:Number
         }
     )
